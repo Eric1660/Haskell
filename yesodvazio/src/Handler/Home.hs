5c9e770b068790a81821f925a40a98cbd3829031
Eric1660 <70780753+Eric1660@users.noreply.github.com>
2021-05-14 15:04:38 -0300

    Add files via upload
    Modificação do Front

--- yesodvazio/src/Handler/Home.hs
+++ yesodvazio/src/Handler/Home.hs
@@ -7,35 +7,41 @@
 module Handler.Home where
 
 import Import
-import Text.Cassius
-import Network.HTTP.Types.Status
-import Database.Persist.Postgresql
+-- import Network.HTTP.Types.Status
+-- import Database.Persist.Postgresql
 
-getPage1R :: Handler Html
-getPage1R = do
-	defaultLayoult $ do
-		addStylesheet (StaticR css_bootstrap_css)
-		toWidgetHead $(cassiusFile "templates/home.cassius")
-		$(whamletFile "templates/page1r.hamlet")
-
-getPage2R :: Handler Html
-getPage2R = do
-	defaultLayoult $ do
-		addStylesheet (StaticR css_bootstrap_css)
-		toWidgetHead $(cassiusFile "templates/home.cassius")
-		$(whamletFile "templates/page2r.hamlet")
-		
-
-menu :: [(Route App, Text)] -> Widget
-menu [] = [whamlet| <h1> ERRO GRAVE |]
-
+-- defaultLayout converte um Widget (front) 
+-- para Handler (back)
 getHomeR :: Handler Html
 getHomeR = do
-    defaultLayoult $ do
-	 -- css/bootstrap.css
+    defaultLayout $ do
+        toWidgetHead [julius|
+            function ola(){
+                alert("ola mundo!");
+            }
+        |]
+        -- css/bootstrap.css
         addStylesheet (StaticR css_bootstrap_css)
-        toWidgetHead $(cassiusFile "templates/home.cassius")
-        $(whamletFile "templates/home.hamlet")
+        toWidgetHead [cassius|
+            h1 
+                color: red;
+        |]
+        [whamlet|
+            <h1>
+                OLA MUNDO
+                
+            <button onclick="ola()"> 
+                Click
+                
+            <img src=@{StaticR img_fatec_png}>
+        |]
 
-        
-        
+
+
+
+
+
+
+
+
+
