d0e205edbe4d88c0ee5eaa0dac3b69896ad8c36c
Eric1660 <70780753+Eric1660@users.noreply.github.com>
2021-05-14 08:49:05 -0300

    Add files via upload
    Modificações no Front e adição de templates.

--- yesodvazio/src/Handler/Home.hs
+++ yesodvazio/src/Handler/Home.hs
@@ -14,40 +14,26 @@ import Database.Persist.Postgresql
 getPage1R :: Handler Html
 getPage1R = do
 	defaultLayoult $ do
-		[whamlet|
-			<a>
-			    Usuário
-		|]
+		addStylesheet (StaticR css_bootstrap_css)
+		toWidgetHead $(cassiusFile "templates/home.cassius")
+		$(whamletFile "templates/page1r.hamlet")
 
 getPage2R :: Handler Html
 getPage2R = do
 	defaultLayoult $ do
-		[whamlet|
-			<a>
-			    Lanches
-		|]
-
-getPage3R :: Handler Html
-getPage3R = do
-	defaultLayoult $ do
-		[whamlet|
-			<a>
-			    Pizza
-		|]
+		addStylesheet (StaticR css_bootstrap_css)
+		toWidgetHead $(cassiusFile "templates/home.cassius")
+		$(whamletFile "templates/page2r.hamlet")
+		
 
 menu :: [(Route App, Text)] -> Widget
-menu [] = [whamlet|
-				<h1>
-					ERRO GRAVE
-        |]
+menu [] = [whamlet| <h1> ERRO GRAVE |]
 
 getHomeR :: Handler Html
 getHomeR = do
     defaultLayoult $ do
 	 -- css/bootstrap.css
         addStylesheet (StaticR css_bootstrap_css)
-
-        
         toWidgetHead $(cassiusFile "templates/home.cassius")
         $(whamletFile "templates/home.hamlet")
 
